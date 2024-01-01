05d2c093946165e8c434b41c2de5c652e8feb99c
nishantb06 <nbhansali06@gmail.com>
2024-01-01 13:06:28 +0530

    feat: Plotting a projectile

--- features/projectiles.go
+++ features/projectiles.go
@@ -16,3 +16,25 @@ func Tick(env Environment, proj Projectile) Projectile {
 	velocity, _ = Add(velocity, env.Wind)
 	return Projectile{position, velocity}
 }
+
+func PlotProjectile(canvas Canvas, proj Projectile) Canvas {
+	x := int(proj.Position.GetX())
+	y := canvas.Height - int(proj.Position.GetY())
+	canvas.Pixels[y][x] = Color_(0, 1, 0)
+	return canvas
+}
+
+func SimulateProjectile(start Tuple, velocity Tuple, multiplier float64, wind Tuple, gravity Tuple, canvasWidth, canvasHeight int, filename string) {
+	velocity, _ = Normalize(velocity)
+	velocity, _ = Multiply(velocity, multiplier)
+	p := Projectile{Position: start, Velocity: velocity}
+	e := Environment{Gravity: gravity, Wind: wind}
+
+	c := Canvas_(canvasWidth, canvasHeight)
+	for p.Position.GetY() > 0 {
+		c = PlotProjectile(c, p)
+		p = Tick(e, p)
+	}
+	ppm := CanvasToPPM(c)
+	PPMToFile(ppm, filename)
+}
--- main/main.go
+++ main/main.go
@@ -4,23 +4,45 @@ import (
 	"fmt"
 
 	"github.com/nishantb06/ray_tracing_challenge_golang/features"
+	"github.com/nishantb06/ray_tracing_challenge_golang/utils"
 )
+
+func sample_conversion() {
+	// Specify the paths for the input PPM file and the output PNG file
+	ppmPath := "projectile.ppm"
+	pngPath := "projectile.png"
+
+	// Convert PPM to PNG
+	if err := utils.ConvertPPMtoPNG(ppmPath, pngPath); err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Conversion successful!")
+}
+
+// func simulateProjectile(start features.Tuple, velocity features.Tuple, multiplier float64, wind features.Tuple, gravity features.Tuple, canvasWidth, canvasHeight int, filename string) {
+// 	velocity, _ = features.Normalize(velocity)
+// 	velocity, _ = features.Multiply(velocity, multiplier)
+// 	p := features.Projectile{Position: start, Velocity: velocity}
+// 	e := features.Environment{Gravity: gravity, Wind: wind}
+
+// 	c := features.Canvas_(canvasWidth, canvasHeight)
+// 	for p.Position.GetY() > 0 {
+// 		c = features.PlotProjectile(c, p)
+// 		p = features.Tick(e, p)
+// 	}
+// 	ppm := features.CanvasToPPM(c)
+// 	features.PPMToFile(ppm, filename)
+// }
 
 func main() {
 	fmt.Println("Hello, World!")
 
-	// c := features.Canvas_(224, 224)
-	// ppm := features.CanvasToPPM(c)
-	// fmt.Println(ppm)
-	// save ppm to file
-	c := features.Canvas_(10, 2)
-	c1 := features.Color_(1, 0.8, 0.6)
-	for i := range c.Pixels {
-		for j := range c.Pixels[i] {
-			c.WritePixel(j, i, c1)
-		}
-	}
-	ppm := features.CanvasToPPM(c)
-	features.PPMToFile(ppm, "canvas.ppm")
+	start := features.Point(0, 1, 0)
+	velocity := features.Vector(1, 1.8, 0)
+	gravity := features.Vector(0, -0.1, 0)
+	wind := features.Vector(-0.01, 0, 0)
 
+	features.SimulateProjectile(start, velocity, 11.25, wind, gravity, 900, 550, "projectile3.ppm")
 }
